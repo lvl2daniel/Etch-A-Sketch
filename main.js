f956910578fca293a508ac35f9fdd7b4c59427ed
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2022-12-23 14:53:42 -0500

    Added Fill On Hover and Resize Button

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
+const sizeChange = document.createElement('button');
+
+
 const wrapper = document.createElement('div');
 const body = document.querySelector('body');
-wrapper.classList = 'wrapper'
+sizeChange.textContent = 'Click to reset & set the size!';
+sizeChange.id = 'input';
+body.append(sizeChange);
+wrapper.id = 'wrapper'
 body.append(wrapper);
-
-
 let i = 0;
+let size = 16;
+let height = (650/size);
+let width = height;
+createGrid(size, height, width);
 
-for (i = 0; i < 16; i++)
-{
-    const row = document.createElement('div');
-    row.classList = 'row';
-    wrapper.appendChild(row)
-    for (let j = 0; j < 16; j++)
+sizeChange.addEventListener('click' , (event) => {
+    deleteGrid(size);
+    size = prompt("What size grid would you like to make? (Max 100 by 100)");
+    if (size > 100)
+    size = 100;
+    size = parseInt(size);
+    height = (650/size);
+    width = height;
+    createGrid(size, height, width);
+
+});
+
+
+function createGrid(size, height, width){
+    for (i = 0; i < size; i++)
     {
-        const item = document.createElement('div');
-        item.classList.add('item');
-        item.addEventListener('mouseover', () => {
-            item.fill();
-        })
-        row.appendChild(item);
+        const row = document.createElement('div');
+        row.id = 'row';
+        wrapper.appendChild(row)
+        for (let j = 0; j < size; j++)
+        {
+            const item = document.createElement('div');
+            item.id = ('item');
+            item.style.width = width + 'px';
+            item.style.height = height + 'px';
+            item.style.padding =
+            item.addEventListener('mouseover', (event) => {
+                console.log("hover works");
+                item.style.backgroundColor = "black";
+            })
+            row.appendChild(item);
+        }
+    }
+}
+
+function deleteGrid(size) {
+    for (i = 0; i < size; i++)
+    {
+        const row = document.getElementById('row');
+        row.remove();
     }
 }
 
 
 function fill()
 {
-    this.classList.add("filled");
+    this.style.backgroundColor = "black";
 }
 
-const hover = document.getElementsByClassName('item');
-hover.addEventListener('mouseover', (e) => {
-    hover.classList.add("filled");
-})
