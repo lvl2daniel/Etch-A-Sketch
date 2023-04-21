4e3d7208125c64bc5159cd05538f8dca3f7853c7
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2023-04-20 20:58:00 -0400

    Add Randomized Color Wheel & Style Changes

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const sizeChange = document.createElement('button');
 const etch = document.createElement('h3');
 const wrapper = document.createElement('div');
+const color = document.createElement('button')
 const body = document.querySelector('body');
+const wrappah2 = document.createElement('div');
+var r = 0;
+var g = 0;
+var b = 0;
+var col = "rgb(" + r + "," + g + "," + b + ")";
+wrappah2.id = 'changer';
 sizeChange.textContent = 'Click to reset & set the size!';
 sizeChange.id = 'input';
+color.id = 'change';
 etch.textContent = 'Etch-a-Sketch!'
 etch.id = 'etch';
 body.append(etch);
-body.append(sizeChange);
-wrapper.id = 'wrapper'
+body.append(wrappah2)
+wrappah2.append(color);
+wrappah2.append(sizeChange);
+wrapper.id = 'wrapper';
 body.append(wrapper);
 let i = 0;
 let size = 16;
 let height = (650/size);
 let width = height;
-createGrid(size, height, width);
+createGrid(size, height, width, col);
 
 sizeChange.addEventListener('click' , (event) => {
     deleteGrid(size);
@@ -24,12 +34,25 @@ sizeChange.addEventListener('click' , (event) => {
     size = parseInt(size);
     height = (650/size);
     width = height;
-    createGrid(size, height, width);
+    createGrid(size, height, width, col);
 
 });
 
+color.addEventListener('click' , (e) =>{
+    deleteGrid(size);
+    col = randomizeColor();
+    createGrid(size, height, width, col);
 
-function createGrid(size, height, width){
+})
+
+function randomizeColor(){
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function createGrid(size, height, width, colour){
     for (i = 0; i < size; i++)
     {
         const row = document.createElement('div');
@@ -44,7 +67,7 @@ function createGrid(size, height, width){
             item.style.padding =
             item.addEventListener('mouseover', (event) => {
                 console.log("hover works");
-                item.style.backgroundColor = "black";
+                item.style.backgroundColor = colour;
             })
             row.appendChild(item);
         }
